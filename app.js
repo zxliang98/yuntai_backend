9b8650297119b1c0af3449ea5c9097a35e344a9e
赵小亮 <1287520001@qq.com>
2020-04-27 14:50:06 +0800

    update:处理跨域

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const express = require('express')
 const router = require('./router/router')
 const app = express()
 
+app.use(cors({
+  origin: ['http://www.zxliang.xyz', 'http://www.zxliang.xyz:3023'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  alloweHeaders: ['Conten-Type', 'application/json;charset=utf-8']
+}))
+
+// //设置跨域访问
+// app.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By",' 3.2.1')
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
+
 // 设置公共文件夹 (static resource such image mp4)
 app.use(express.static(path.join(__dirname, 'assets')))
 // app.use(express.static(path.join(__dirname, 'public')))
@@ -14,8 +30,6 @@ app.use(bodyParser.json({ limit: '50mb' }))
 
 // 获取解析application/x-www-form-urlencoded类型的body
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
-
-app.use(cors())
 
 app.use('/api', router)
 
